42b2525b61e6e55e4ce488e179c1e21b9dfb8512
MASTER <donridextech@gmail.com>
2023-02-09 13:18:22 +0100

    first commit

---
+++ ridex.js
@@ -0,0 +1,37 @@
+var ridex =document.createElement('h1')
+ridex.textContent='DON RIDEX TECHNOLOGY'
+document.body.appendChild(ridex)
+
+var personArr =[
+    {
+        "personId" : 123,
+        "name" : "john",
+        "city" : "cameroun",
+        "phoneNo" : "08083919800"
+    },
+    {
+    "personId": 1234,
+        "name": "john doe",
+        "city" : "cameroun",
+        "phoneNo" : "08083919800"
+    },
+    {
+    "personId" : 123,
+        "name" : "john",        "phoneNo" : "08083919800"
+},
+{
+        "personId" : 123,
+        "name" : "john",
+        "city" : "cameroun",
+        "phoneNo" : "08083919800",
+rrr}
+];
+console.table(personArr, ['name', 'personId', 'city']);
+ var caseOtv = 'have you done your assignment??';
+ 
+
+ var mannyblack= ['dog', 'cat', 'bird', 'cock', 'horse', 'cow']
+ mannyblack.push('fish', 'meat')
+var city= prompt('enter your city of choice')
+city = city.toUpperCase();
+Element.toUpperCase();
