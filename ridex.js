aa3c43cf3d3b43caabb72a5df3b8d9780a04d0eb
MASTER <donridextech@gmail.com>
2023-03-04 13:46:53 +0100

    mycourses commit

--- ridex.js
+++ ridex.js
@@ -1,37 +1,10 @@
-var ridex =document.createElement('h1')
-ridex.textContent='DON RIDEX TECHNOLOGY'
-document.body.appendChild(ridex)
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul")
 
-var personArr =[
-    {
-        "personId" : 123,
-        "name" : "john",
-        "city" : "cameroun",
-        "phoneNo" : "08083919800"
-    },
-    {
-    "personId": 1234,
-        "name": "john doe",
-        "city" : "cameroun",
-        "phoneNo" : "08083919800"
-    },
-    {
-    "personId" : 123,
-        "name" : "john",        "phoneNo" : "08083919800"
-},
-{
-        "personId" : 123,
-        "name" : "john",
-        "city" : "cameroun",
-        "phoneNo" : "08083919800",
-rrr}
-];
-console.table(personArr, ['name', 'personId', 'city']);
- var caseOtv = 'have you done your assignment??';
- 
+button.addEventListener("click", () =>{
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li)
+})
 
- var mannyblack= ['dog', 'cat', 'bird', 'cock', 'horse', 'cow']
- mannyblack.push('fish', 'meat')
-var city= prompt('enter your city of choice')
-city = city.toUpperCase();
-Element.toUpperCase();
